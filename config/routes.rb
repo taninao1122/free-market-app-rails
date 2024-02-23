6f34276a7df4e469cd935f0326f9eca6a9767c72
taninao1122 <tani.naoya1122@gmail.com>
2024-02-23 15:58:26 +0900

    商品一覧ページへの遷移

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'products/index' #TODO
-  get 'products/show'#TODO
+  get 'products' => "products#index" #TODO
+  # get 'products/show'#TODO
+
   root 'home#top' 
   get 'home/top' => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
