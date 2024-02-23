9c9457f911d310f539e4e82f19a10f398366632c
taninao1122 <tani.naoya1122@gmail.com>
2024-02-23 18:36:03 +0900

    ログイン・ログアウト用の仮画面を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   get 'products' => "products#show" #TODO
   get 'sell' => "products#sell"
-
+  get 'profile' => "user#profile"
+  get 'login' => "user#login"
+  get 'logout' => "user#logout"
+  
   root 'home#top' 
   get 'home/top' => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
