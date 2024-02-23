1055b283d3cda233edebd26c05e0d71808339e88
taninao1122 <tani.naoya1122@gmail.com>
2024-02-23 14:35:55 +0900

    商品一覧を作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index' #TODO
+  get 'products/show'#TODO
+  root 'home#top' 
+  get 'home/top' => "home#top"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+
 end
