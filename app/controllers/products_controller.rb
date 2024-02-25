80becd38d89f7fc82af69c1bdebcec5a9d40b9e9
taninao1122 <tani.naoya1122@gmail.com>
2024-02-25 23:10:44 +0900

    CRUD処理を実装

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,25 @@ class ProductsController < ApplicationController
   def show
     @products = Product.all
   end
+
+  def register
+    # 登録作成用
+  end
+
+  def create
+    # 登録用
+    product = Product.new(name:  params['name'], price: params['price'])
+    product.save
+    redirect_to('/home/top')
+  end
+  
+  def update
+    # 登録更新用
+  end
+
+  def delete
+    # 削除用
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
-  get 'products' => "products#show" #TODO
-  get 'sell' => "products#sell"
-  get 'profile' => "user#profile"
-  get 'login' => "user#login"
-  get 'logout' => "user#logout"
+  get 'products' => "products#show" #商品一覧表示
+  get 'sell' => "products#sell" # 出品画面表示
+  get 'profile' => "user#profile" # マイプロフィール表示
+  get 'login' => "user#login" # ログイン表示
+  get 'logout' => "user#logout" # ログアウト表示
+
+  post 'products/create' => "products#create"
   
   root 'home#top' 
   get 'home/top' => "home#top"
