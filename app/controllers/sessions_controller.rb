933ca12ebd83bdeaaeb62b74959370fc27ae6c88
taninao1122 <tani.naoya1122@gmail.com>
2024-03-10 16:20:12 +0900

    flash機能でメッセージを表示

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,9 +6,11 @@ class SessionsController < ApplicationController
       # TODO パスワードのハッシュ化
       user = User.find_by(email: params[:email], password: params[:password])
       if user.nil?
+        flash.now[:notice] = "EmailまたはPasswordの値が不正です"
         render("sessions#new")
       else
         session[:user_id] = user.id
+        flash[:notice] = "ログインに成功しました"
         redirect_to '/home/top'
       end
     end
@@ -16,6 +18,7 @@ class SessionsController < ApplicationController
     def destory
       if session[:user_id]
         session[:user_id] = nil
+        flash[:notice] = "ログアウトに成功しました"
         redirect_to '/home/top'
       end
     end
