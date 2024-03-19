beaf061c71a9d82340bb307e01beef55a4e5ef9b
taninao1122 <tani.naoya1122@gmail.com>
2024-03-19 23:46:07 +0900

    プロフィール画像を編集できるように修正

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,28 @@
 class UserController < ApplicationController
   def profile
-    
   end
 
   def edit
-    @user = User.new()
+    @user = User.find_by(id: @current_user.id)
   end
 
+  def update
+    user = User.find_by(id: @current_user.id)
+    user.name = params['name']
+    if params[:image]
+      image = params[:image]
+      user.profile_image = "#{user.id}.png"
+      File.binwrite("public/profile/#{user.profile_image}", image.read)
+    else
+      user.profile_image = "default.png"
+    end
+
+    if user.save
+      flash[:notice] = "プロフィールの編集に成功しました"
+      redirect_to("/profile/#{user.id}")
+    else
+      flash.now[:notice] = "プロフィールの編集に失敗しました"
+      render("profile/#{user.id}/edit")
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,17 @@
 Rails.application.routes.draw do
+  get 'products' => "products#index"
+  get 'products/:id/detail' => "products#detail"
   post 'products/create' => "products#create" # マイプロフィール表示
   post 'products/:id/delete' => "products#destory"
 
   get 'profile/:id' => "user#profile" # マイプロフィール表示
   get 'profile/:id/edit' => "user#edit"
+  post 'profile/:id/update' => "user#update"
 
   # リソースを使ってルーティングを変更する
   get 'sell' => "products#sell" # 出品画面表示
   get 'login' => "sessions#new" # ログイン
   get 'logout' => "sessions#destory" # ログアウト
-  get 'products' => "products#index"
-  get 'products/:id/detail' => "products#detail"
-
   post 'login/create' => "sessions#create" # ログイン表示
 
   root 'home#top' 
