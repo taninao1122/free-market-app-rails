03efb3b5db4531599e8c79dc12f3649da17e61fa
taninao1122 <tani.naoya1122@gmail.com>
2024-03-25 00:12:10 +0900

    モデルクラスのバリデーションを記載

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+    belongs_to :users
+    validates :name, presence: true, length: {maximum: 64}
+    validates :price, presence: true
+    validates :description, length: {maximum: 256}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :name, length: {maximum: 64}, presence: true
+    validates :email, length: {in: 3..32}, presence: true
+    validates :password, length: { in: 6..20 }, presence: true
+    validates :profile_image, presence: true
+
+    #TODO passwordを暗号化
 end
